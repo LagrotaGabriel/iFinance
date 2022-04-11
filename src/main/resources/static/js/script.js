500da1e3d5f84c7108cc5277c12451ebf79025d3
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-04-11 01:10:09 -0300

    BACKUP

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -5,131 +5,6 @@ window.onresize = doALoadOfStuff;
 
 function responsive(bodyHeight, bodyWidth){
 
-	var textos = document.getElementsByClassName('txt');
-
-	if(bodyWidth > 1200){
-		console.log('Muito grande')
-		document.getElementById('side_menu').hidden=false;
-		document.getElementById('menu_superior').hidden=false;
-		document.getElementById('menu_superior_responsivo').hidden=true;
-		document.getElementById('titulo_linha_geral_clicavel1').style.fontSize="1.5rem";
-		document.getElementById('titulo_linha_geral_clicavel2').style.fontSize="1.5rem";
-		document.getElementById('titulo_linha_geral_clicavel3').style.fontSize="1.5rem";
-		document.getElementById('titulo_linha_geral_clicavel4').style.fontSize="1.5rem";
-
-		document.getElementById('titulo_linha_geral_clicavel1').style.padding="20px 15px";
-		document.getElementById('titulo_linha_geral_clicavel2').style.padding="20px 15px";
-		document.getElementById('titulo_linha_geral_clicavel3').style.padding="20px 15px";
-		document.getElementById('titulo_linha_geral_clicavel4').style.padding="20px 15px";
-
-		document.getElementById('titulo_linha_geral_clicavel1').style.textAlign="center";
-		document.getElementById('titulo_linha_geral_clicavel2').style.textAlign="center";
-		document.getElementById('titulo_linha_geral_clicavel3').style.textAlign="center";
-		document.getElementById('titulo_linha_geral_clicavel4').style.textAlign="center";		
-		for (var i = 0; i < textos.length; i++) {
-  			var texto = textos[i];
-  			texto.style.fontSize = "1.25rem";
-		}
-	}
-	else if(bodyWidth <= 1200 && bodyWidth > 992){
-		console.log('Grande');
-		document.getElementById('side_menu').hidden=false;
-		document.getElementById('menu_superior').hidden=false;
-		document.getElementById('menu_superior_responsivo').hidden=true;
-		document.getElementById('titulo_linha_geral_clicavel1').style.fontSize="1.5rem";
-		document.getElementById('titulo_linha_geral_clicavel2').style.fontSize="1.5rem";
-		document.getElementById('titulo_linha_geral_clicavel3').style.fontSize="1.5rem";
-		document.getElementById('titulo_linha_geral_clicavel4').style.fontSize="1.5rem";
-
-		document.getElementById('titulo_linha_geral_clicavel1').style.padding="20px 15px";
-		document.getElementById('titulo_linha_geral_clicavel2').style.padding="20px 15px";
-		document.getElementById('titulo_linha_geral_clicavel3').style.padding="20px 15px";
-		document.getElementById('titulo_linha_geral_clicavel4').style.padding="20px 15px";
-
-		document.getElementById('titulo_linha_geral_clicavel1').style.textAlign="center";
-		document.getElementById('titulo_linha_geral_clicavel2').style.textAlign="center";
-		document.getElementById('titulo_linha_geral_clicavel3').style.textAlign="center";
-		document.getElementById('titulo_linha_geral_clicavel4').style.textAlign="center";		
-		for (var i = 0; i < textos.length; i++) {
-  			var texto = textos[i];
-  			texto.style.fontSize = "1rem";
-		}
-	}
-	else if(bodyWidth <= 992 && bodyWidth > 768){
-		console.log('Médio');
-		document.getElementById('side_menu').hidden=true;
-		document.getElementById('menu_superior').hidden=true;
-		document.getElementById('menu_superior_responsivo').hidden=false;
-		document.getElementById('titulo_linha_geral_clicavel1').style.fontSize="1.5rem";
-		document.getElementById('titulo_linha_geral_clicavel2').style.fontSize="1.5rem";
-		document.getElementById('titulo_linha_geral_clicavel3').style.fontSize="1.5rem";
-		document.getElementById('titulo_linha_geral_clicavel4').style.fontSize="1.5rem";
-
-		document.getElementById('titulo_linha_geral_clicavel1').style.padding="10px 15px";
-		document.getElementById('titulo_linha_geral_clicavel2').style.padding="10px 15px";
-		document.getElementById('titulo_linha_geral_clicavel3').style.padding="10px 15px";
-		document.getElementById('titulo_linha_geral_clicavel4').style.padding="10px 15px";
-
-		document.getElementById('titulo_linha_geral_clicavel1').style.textAlign="center";
-		document.getElementById('titulo_linha_geral_clicavel2').style.textAlign="center";
-		document.getElementById('titulo_linha_geral_clicavel3').style.textAlign="center";
-		document.getElementById('titulo_linha_geral_clicavel4').style.textAlign="center";
-		for (var i = 0; i < textos.length; i++) {
-  			var texto = textos[i];
-  			texto.style.fontSize = "0.95rem";
-		}
-	}
-	else if(bodyWidth <= 768 && bodyWidth > 540){
-		console.log('Pequeno');
-		document.getElementById('side_menu').hidden=true;
-		document.getElementById('menu_superior').hidden=true;
-		document.getElementById('menu_superior_responsivo').hidden=false;
-		document.getElementById('titulo_linha_geral_clicavel1').style.fontSize="1rem";
-		document.getElementById('titulo_linha_geral_clicavel2').style.fontSize="1rem";
-		document.getElementById('titulo_linha_geral_clicavel3').style.fontSize="1rem";
-		document.getElementById('titulo_linha_geral_clicavel4').style.fontSize="1rem";
-
-		document.getElementById('titulo_linha_geral_clicavel1').style.padding="7px";
-		document.getElementById('titulo_linha_geral_clicavel2').style.padding="7px";
-		document.getElementById('titulo_linha_geral_clicavel3').style.padding="7px";
-		document.getElementById('titulo_linha_geral_clicavel4').style.padding="7px";
-
-		document.getElementById('titulo_linha_geral_clicavel1').style.textAlign="left";
-		document.getElementById('titulo_linha_geral_clicavel2').style.textAlign="left";
-		document.getElementById('titulo_linha_geral_clicavel3').style.textAlign="left";
-		document.getElementById('titulo_linha_geral_clicavel4').style.textAlign="left";
-
-		for (var i = 0; i < textos.length; i++) {
-  			var texto = textos[i];
-  			texto.style.fontSize = "0.90rem";
-		}
-	}
-	else if(bodyWidth < 540){
-		console.log('Muito pequeno');
-		document.getElementById('side_menu').hidden=true;
-		document.getElementById('menu_superior').hidden=true;
-		document.getElementById('menu_superior_responsivo').hidden=false;
-		document.getElementById('titulo_linha_geral_clicavel1').style.fontSize="0.75rem";
-		document.getElementById('titulo_linha_geral_clicavel2').style.fontSize="0.75rem";
-		document.getElementById('titulo_linha_geral_clicavel3').style.fontSize="0.75rem";
-		document.getElementById('titulo_linha_geral_clicavel4').style.fontSize="0.75rem";
-
-		document.getElementById('titulo_linha_geral_clicavel1').style.padding="5px";
-		document.getElementById('titulo_linha_geral_clicavel2').style.padding="5px";
-		document.getElementById('titulo_linha_geral_clicavel3').style.padding="5px";
-		document.getElementById('titulo_linha_geral_clicavel4').style.padding="5px";
-
-		document.getElementById('titulo_linha_geral_clicavel1').style.textAlign="center";
-		document.getElementById('titulo_linha_geral_clicavel2').style.textAlign="center";
-		document.getElementById('titulo_linha_geral_clicavel3').style.textAlign="center";
-		document.getElementById('titulo_linha_geral_clicavel4').style.textAlign="center";
-
-		for (var i = 0; i < textos.length; i++) {
-  			var texto = textos[i];
-  			texto.style.fontSize = "0.80rem";
-		}
-	}
-
 	/* Ajustando content */
 	var tamanhoMain = document.getElementById('main');
 	var tamanhoMenuSuperior = document.getElementById('menu_superior');
@@ -140,20 +15,89 @@ function responsive(bodyHeight, bodyWidth){
 	var tamanhoInfosPerfil1 = document.getElementById('infos_perfil1');
 	var tamanhoInfosPerfil2 = document.getElementById('infos_perfil2');
 	var tituloLinhaGeralClicavel = document.getElementById('titulo_linha_geral_clicavel');
+	var tamanhoInformativos = document.getElementById('informativos');
 
+	tamanhoConteudoContainer.style.height = parseInt(parseInt(tamanhoConteudoTitulo.clientHeight) + parseInt(informativos.clientHeight) + 30) + 'px';
 
 	if(tamanhoMenuSuperior.clientHeight == 0){
-		console.log('SEM MENU SUPERIOR');
-		tamanhoConteudoContainer.style.height = parseInt(parseInt(tamanhoMain.clientHeight) - parseInt(tamanhoMenuResponsivo.clientHeight) - parseInt(tamanhoTituloPathContainer.clientHeight)) - 35 + 'px';
-		tamanhoInfosPerfil1.style.height = (parseInt(parseInt(tamanhoConteudoContainer.clientHeight) - parseInt(tamanhoConteudoTitulo.clientHeight)) - 0 + 'px');
-		tamanhoInfosPerfil2.style.height = (parseInt(parseInt(tamanhoConteudoContainer.clientHeight) - parseInt(tamanhoConteudoTitulo.clientHeight)) - 0 + 'px');
+		tamanhoMain.style.height=parseInt(parseInt(tamanhoConteudoContainer.clientHeight + parseInt(tamanhoMenuResponsivo)) + 'px');
 	}
 	else{
-		console.log('COM MENU SUPERIOR');
-		tamanhoConteudoContainer.style.height = parseInt(parseInt(tamanhoMain.clientHeight) - parseInt(tamanhoMenuSuperior.clientHeight) - parseInt(tamanhoTituloPathContainer.clientHeight)) - 35 + 'px';
-		tamanhoInfosPerfil1.style.height = (parseInt(parseInt(tamanhoConteudoContainer.clientHeight) - parseInt(tamanhoConteudoTitulo.clientHeight)) - 0 + 'px');
-		tamanhoInfosPerfil2.style.height = (parseInt(parseInt(tamanhoConteudoContainer.clientHeight) - parseInt(tamanhoConteudoTitulo.clientHeight)) - 0 + 'px');
+		tamanhoMain.style.height=parseInt(parseInt(tamanhoConteudoContainer.clientHeight + parseInt(tamanhoMenuSuperior)) + 'px');
 	}
+
+	var imagens = document.getElementsByClassName('sidebar_menu_img');
+	var imagens_title = document.getElementsByClassName('aside_title');
+	var menu_responsivo_item = document.getElementsByClassName('sup_sup');
+
+	if(bodyWidth > 1200){
+		console.log('Muito grande')
+		tamanhoMain.style.paddingBottom="0px";
+		document.getElementById('side_menu').hidden=false;
+		document.getElementById('menu_superior').hidden=false;
+		document.getElementById('menu_superior_responsivo').hidden=true;
+		document.getElementById('conteudo_titulo_text').style.fontSize="2rem";
+		for(var i = 0; i < imagens.length; i++){
+			imagens[i].style.height="50%";
+			
+		}
+		for(var j = 0; j < imagens_title.length; j++){
+			imagens_title[j].style.fontSize="0.85rem";
+		}
+
+	}
+	else if(bodyWidth <= 1200 && bodyWidth > 992){
+		console.log('Grande');
+		tamanhoMain.style.paddingBottom="0px";
+		document.getElementById('side_menu').hidden=false;
+		document.getElementById('menu_superior').hidden=false;
+		document.getElementById('menu_superior_responsivo').hidden=true;
+		document.getElementById('conteudo_titulo_text').style.fontSize="2rem";
+
+		for(var i = 0; i < imagens.length; i++){
+			imagens[i].style.height="45%";
+			
+		}
+		for(var j = 0; j < imagens_title.length; j++){
+			imagens_title[j].style.fontSize="0.75rem";
+		}
+
+	}
+	else if(bodyWidth <= 992 && bodyWidth > 768){
+		console.log('Médio');
+		tamanhoMain.style.paddingBottom="0px";
+		document.getElementById('side_menu').hidden=true;
+		document.getElementById('menu_superior').hidden=true;
+		document.getElementById('menu_superior_responsivo').hidden=false;
+		document.getElementById('conteudo_titulo_text').style.fontSize="2rem";
+		for(var i = 0; i < menu_responsivo_item.length; i++){
+			menu_responsivo_item[i].style.fontSize="0.85rem";
+		}
+	}
+	else if(bodyWidth <= 768 && bodyWidth > 540){
+		console.log('Pequeno');
+		tamanhoMain.style.paddingBottom="30px";
+		document.getElementById('side_menu').hidden=true;
+		document.getElementById('menu_superior').hidden=true;
+		document.getElementById('menu_superior_responsivo').hidden=false;
+		document.getElementById('conteudo_titulo_text').style.fontSize="1.5rem";
+		for(var i = 0; i < menu_responsivo_item.length; i++){
+			menu_responsivo_item[i].style.fontSize="0.75rem";
+		}
+	}
+	else if(bodyWidth < 540){
+		console.log('Muito pequeno');
+		tamanhoMain.style.paddingBottom="30px";
+		document.getElementById('side_menu').hidden=true;
+		document.getElementById('menu_superior').hidden=true;
+		document.getElementById('menu_superior_responsivo').hidden=false;
+		document.getElementById('conteudo_titulo_text').style.fontSize="1.25rem";
+		for(var i = 0; i < menu_responsivo_item.length; i++){
+			menu_responsivo_item[i].style.fontSize="0.65rem";
+		}
+	}
+
+
 }
 
 function doALoadOfStuff() {
@@ -229,5 +173,94 @@ function returnMenuState(item){
 	}
 	else if(item == "configuracoes"){
 		document.getElementById("img_aside_configuracoes").src="../static/img/settingsblue.png";
+	}
+}
+
+/* =================== INTERAÇÕES COM O CONTEÚDO =================== */
+
+function animateContent(item){
+
+	if(item == 1){
+		document.getElementById("maisinfo_item_rendamensal").style.background="#00CCFF";
+		document.getElementById("maisinfo_item_rendamensal_clicavel").style.color="#121212";
+		document.getElementById("maisinfo_item_rendamensal_clicavel").style.transition="1s";
+		document.getElementById("maisinfo_item_rendamensal").style.transition="1s";
+		document.getElementById("conteudo_item_total_rendamensal").style.cursor="pointer";
+	}
+	else if(item == 2){
+		document.getElementById("maisinfo_item_despesas").style.background="#00CCFF";
+		document.getElementById("maisinfo_item_despesas_clicavel").style.color="#121212";
+		document.getElementById("maisinfo_item_despesas_clicavel").style.transition="1s";
+		document.getElementById("maisinfo_item_despesas").style.transition="1s";
+		document.getElementById("conteudo_item_total_despesas").style.cursor="pointer";
+	}
+	else if(item == 3){
+		document.getElementById("maisinfo_item_contasapagar").style.background="#00CCFF";
+		document.getElementById("maisinfo_item_contasapagar_clicavel").style.color="#121212";
+		document.getElementById("maisinfo_item_contasapagar_clicavel").style.transition="1s";
+		document.getElementById("maisinfo_item_contasapagar").style.transition="1s";
+		document.getElementById("conteudo_item_total_contasapagar").style.cursor="pointer";
+	}
+	else if(item == 4){
+		document.getElementById("maisinfo_item_recebido").style.background="#00CCFF";
+		document.getElementById("maisinfo_item_recebido_clicavel").style.color="#121212";
+		document.getElementById("maisinfo_item_recebido_clicavel").style.transition="1s";
+		document.getElementById("maisinfo_item_recebido").style.transition="1s";
+		document.getElementById("conteudo_item_total_recebido").style.cursor="pointer";
+	}
+	else if(item == 5){
+		document.getElementById("maisinfo_item_receber").style.background="#00CCFF";
+		document.getElementById("maisinfo_item_receber_clicavel").style.color="#121212";
+		document.getElementById("maisinfo_item_receber_clicavel").style.transition="1s";
+		document.getElementById("maisinfo_item_receber").style.transition="1s";
+		document.getElementById("conteudo_item_total_receber").style.cursor="pointer";
+	}
+	else if(item == 6){
+		document.getElementById("maisinfo_item_contasatrasadas").style.background="#00CCFF";
+		document.getElementById("maisinfo_item_contasatrasadas_clicavel").style.color="#121212";
+		document.getElementById("maisinfo_item_contasatrasadas_clicavel").style.transition="1s";
+		document.getElementById("maisinfo_item_contasatrasadas").style.transition="1s";
+		document.getElementById("conteudo_item_total_contasatrasadas").style.cursor="pointer";
+	}
+
+}
+
+function returnContentState(item){
+
+	if(item == 1){
+		document.getElementById("maisinfo_item_rendamensal").style.background="transparent";
+		document.getElementById("maisinfo_item_rendamensal_clicavel").style.color="#FFFFFF";
+		document.getElementById("maisinfo_item_rendamensal_clicavel").style.transition="1s";
+		document.getElementById("maisinfo_item_rendamensal").style.transition="1s";
+	}
+	else if(item == 2){
+		document.getElementById("maisinfo_item_despesas").style.background="transparent";
+		document.getElementById("maisinfo_item_despesas_clicavel").style.color="#FFFFFF";
+		document.getElementById("maisinfo_item_despesas_clicavel").style.transition="1s";
+		document.getElementById("maisinfo_item_despesas").style.transition="1s";
+	}
+	else if(item == 3){
+		document.getElementById("maisinfo_item_contasapagar").style.background="transparent";
+		document.getElementById("maisinfo_item_contasapagar_clicavel").style.color="#FFFFFF";
+		document.getElementById("maisinfo_item_contasapagar_clicavel").style.transition="1s";
+		document.getElementById("maisinfo_item_contasapagar").style.transition="1s";
+	}
+	else if(item == 4){
+		document.getElementById("maisinfo_item_recebido").style.background="transparent";
+		document.getElementById("maisinfo_item_recebido_clicavel").style.color="#FFFFFF";
+		document.getElementById("maisinfo_item_recebido_clicavel").style.transition="1s";
+		document.getElementById("maisinfo_item_recebido").style.transition="1s";
+	}
+	else if(item == 5){
+		document.getElementById("maisinfo_item_receber").style.background="transparent";
+		document.getElementById("maisinfo_item_receber_clicavel").style.color="#FFFFFF";
+		document.getElementById("maisinfo_item_receber_clicavel").style.transition="1s";
+		document.getElementById("maisinfo_item_receber").style.transition="1s";
+	}
+	else if(item == 6){
+		document.getElementById("maisinfo_item_contasatrasadas").style.background="transparent";
+		document.getElementById("maisinfo_item_contasatrasadas_clicavel").style.color="#FFFFFF";
+		document.getElementById("maisinfo_item_contasatrasadas_clicavel").style.transition="1s";
+		document.getElementById("maisinfo_item_contasatrasadas").style.transition="1s";
 	}
 }
