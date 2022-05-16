9d4ff7ff9a4bb48cda689fdd9b5811411826dc2b
LagrotaGabriel <gabriellagrota23@gmail.com>
2022-05-16 17:51:32 -0300

    *** Criei o front end da tela de perfil *** Criei os end points e o controller da tela de perfil -> Falta construir a responsividade da tela e adequar o end point de edição do usuário ==================================================== *** I created the profile screen front end *** I created the profile screen's end points and controller -> It remains to build the screen's responsiveness and adjust the user's editing end point

---
+++ src/main/java/br/com/ifinance/controllers/PerfilController.java
@@ -0,0 +1,39 @@
+package br.com.ifinance.controllers;
+
+import br.com.ifinance.models.entities.User;
+import br.com.ifinance.repositories.UserRepository;
+import br.com.ifinance.utils.Utils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping
+public class PerfilController {
+
+    Utils utils = new Utils();
+
+    @Autowired
+    UserRepository userRepository;
+
+    @GetMapping("/{username}")
+    public ModelAndView perfilControllerGet(@PathVariable String username, User user,
+                                            ModelAndView modelAndView, ModelMap modelMap){
+
+        modelMap.addAttribute("usuario", utils.loggedUser(userRepository));
+        modelAndView.setViewName("perfil");
+        return modelAndView;
+    }
+
+    @PostMapping("/{username}")
+    public ModelAndView perfilControllerPost(@PathVariable String username, User user, ModelAndView modelAndView){
+
+        modelAndView.setViewName("redirect:perfil");
+        return modelAndView;
+    }
+}
